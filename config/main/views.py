176b26a2a6eccae104583626ac408ce7782e454d
reza drakhshan <github2reza@gmail.com>
2024-11-09 21:33:26 +0330

    feat
    continue contact us : develop html design for email message

--- config/main/views.py
+++ config/main/views.py
@@ -18,17 +18,27 @@ def home(request):
 def contact_us(request):
     if request.method == "POST":
         name = request.POST.get("name")
-        email = request.POST.get("email")
+        user_email = request.POST.get("email")
         subject = request.POST.get("subject")
         message = request.POST.get("message")
         html_content = render_to_string(
             "email/contact_email.html",
-            {"name": name, "email": email, "subject": subject, "message": message},
+            {"name": name, "email": user_email, "subject": subject, "message": message},
         )
         text_content = strip_tags(html_content)
-        email = EmailMultiAlternatives(subject, text_content, EMAIL_HOST_USER, [EMAIL_HOST_USER])
+        email = EmailMultiAlternatives(
+            subject, text_content, EMAIL_HOST_USER, [EMAIL_HOST_USER]
+        )
         email.attach_alternative(html_content, "text/html")
         email.send()
-        messages.success(request,"از اینکه با ما تماس گرفتید متشکریم!")
-        return redirect("main:contact_us")
+        html_content = render_to_string(
+            "email/contact_user_message.html"
+        )
+        text_content = strip_tags(html_content)
+        email2 = EmailMultiAlternatives(
+            "پیام شما دریافت شد - از ارتباط شما سپاسگزاریم!", text_content, EMAIL_HOST_USER, [user_email]
+        )
+        email2.attach_alternative(html_content, "text/html")
+        email2.send()
+        return render(request, "email/mail-success.html")
     return render(request, "contact-us.html")
