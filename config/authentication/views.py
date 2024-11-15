da03cc2222ada480c634d280958e6ace48e5f35d
reza drakhshan <github2reza@gmail.com>
2024-11-15 09:26:14 +0330

    feat
    continue authentication : complete register function view

--- config/authentication/views.py
+++ config/authentication/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+from django.contrib import auth
 
 # Create your views here.
 
@@ -10,13 +12,21 @@ def login(request):
 
 
 def register(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        user_object = User.objects.create(
+            username=name, email=email, password=make_password(password)
+        )
+        user_object.save()
+        auth.login(request, user_object)
+        return redirect("main:home")
+
     return render(request, "authentication/register.html")
 
 
-
 def check_email(request):
-    email = request.GET.get('email', None)
-    response = {
-        'exists': User.objects.filter(email=email).exists()
-    }
+    email = request.GET.get("email", None)
+    response = {"exists": User.objects.filter(email=email).exists()}
     return JsonResponse(response)
