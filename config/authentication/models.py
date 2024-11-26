31d2a9da233f56e4ff89e526a82a7e74f01cf736
reza drakhshan <github2reza@gmail.com>
2024-11-26 21:29:02 +0330

    feat
    Continue Profile page : Create a model for Profile

--- config/authentication/models.py
+++ config/authentication/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, unique=True, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=11)
+    address = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="user/profile/")
+    slug = models.UUIDField(primary_key=True, default=uuid.uuid4)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
